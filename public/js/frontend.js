cf25e9eb457bf38003a91946298f8a565bbd0f31
Charles Srstka <support@charlessoft.com>
2018-07-28 23:45:39 -0500

    fix the bloody damn HTTP link for jquery

--- public/js/frontend.js
+++ public/js/frontend.js
@@ -6,7 +6,7 @@ function addComment(event) {
   const articleID = $(this).closest('article').attr('article_id');
   const username = $(this).find('[name="username"]').val();
   const body = $(this).find('[name="comment"]').val();
-  console.log('okay wtffff');
+
   $.ajax({
     method: 'POST',
     url: '/api/comment',
@@ -34,7 +34,6 @@ function deleteComment(event) {
 }
 
 $(document).ready(() => {
-  console.log('get here?');
   $('.comment-form').submit(addComment);
   $('.comment-delete').click(deleteComment);
 });
